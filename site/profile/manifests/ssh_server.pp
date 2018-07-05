4e467fbcaab746211ae2b6611d0a8c1bda7774a5
sjc11221 <stevejchang@gmail.com>
2018-07-05 10:49:52 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => ' AAAAB3NzaC1yc2EAAAADAQABAAABAQDXI79Duf1WJuOp4waI6xDavHlQOQzXv0FWJxDAyDU5Ef0TKeuL7QdZpkuA5l2l8qcf4iSrvC/WPb3rHKu/tWgn0QbfHYWC0rVJ/ZAO2mSrxwLAbYz3kCZmdJBld6oMhjz4stlb3q0zQ32NiBx4hY9PUfNqKRn9goRw7agPxmJFNAPXfiTULen5tgPSWRnpQ3nmpBxiX5z1w7CmPYafEApVTiop5EDISwiqKqo+kCoYJFUBI1pkF2SEGu4U4bX3Zk5uxhC/YJCmSi3RkDRQqUIc1MLANtrnhBlTl+ZVAr2Kwe2DlFQZRjYUaoms4iGlbmedqCMG/BKqdVDOMRlVKhV5',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDXI79Duf1WJuOp4waI6xDavHlQOQzXv0FWJxDAyDU5Ef0TKeuL7QdZpkuA5l2l8qcf4iSrvC/WPb3rHKu/tWgn0QbfHYWC0rVJ/ZAO2mSrxwLAbYz3kCZmdJBld6oMhjz4stlb3q0zQ32NiBx4hY9PUfNqKRn9goRw7agPxmJFNAPXfiTULen5tgPSWRnpQ3nmpBxiX5z1w7CmPYafEApVTiop5EDISwiqKqo+kCoYJFUBI1pkF2SEGu4U4bX3Zk5uxhC/YJCmSi3RkDRQqUIc1MLANtrnhBlTl+ZVAr2Kwe2DlFQZRjYUaoms4iGlbmedqCMG/BKqdVDOMRlVKhV5',
   }  
 }
