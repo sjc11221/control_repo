c8921441fcf8b6049d54eabc3460892d413214d7
sjc11221 <stevejchang@gmail.com>
2018-07-05 10:40:10 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,8 +8,8 @@ node default {
 node 'master.puppet.vm' {
   include role::master_server
   file { '/root/README':
-    ensure ==> file,
-    content ==> $fqdn,
+    ensure => file,
+    content => $fqdn,
   }
 }
 
